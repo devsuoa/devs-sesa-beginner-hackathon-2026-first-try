7df94efe63a2ce52c0a09a6741b111456553e629
Finley Griffiths <yelnifgames@gmail.com>
2026-04-18 19:31:07 +1200

    Initial commit of ids, questions, and answers

---
+++ fastapi/questions_and_answers.py
@@ -0,0 +1,25 @@
+"""File for questions and answers for game. Format: {ID: [question, answer]}"""
+
+qa_dict = {
+    1: ["Mayday! Mayday! We are on our way to Orphion Lux from Zenthara Expanse but we are running out of fuel. Where can we stop to refuel our spaceship?", "Crysalune Abyss"],
+    2: ["My space pet Bort is missing, he's super unfriendly and I think he might attack someone! He LOVES hot winds, which planet might he be on!?", "Mackelvie"],
+    3: ["My steering wheel isn't working, there's a code 1011, what does it mean???? There's an asteroid in front of me and I NEED help ASAP!!!!", "Eject your vehicle"],
+    4: ["I'm severely allergic to alien fish and I just swallowed it. WHERE SHOULD I GO???", "Taurino"],
+    5: ["My grandma hasn't responded to me in 5 minutes, she lives on the biggest planet and I forgot what it's called pls help!!!", "Feradris"],
+    6: ["Late for work and my vehicle isn't working. There's a code 0000 on the dashboard, help?", "Your vehicle is off"],
+    7: ["The engine broke down mid transport, I need to order a new part asap or I'll miss my wedding. Where should I order it?", "Feradris"],
+    8: ["My flight is in 10 minutes and I just realized I got a spacemail. What the heck does a curve with a dot inside mean???", "You are not permitted to travel"],
+    9: ["My mom and dad are retiring soon. They're Rulix and they love sunbathing. Which planet would be most suitable for them?", "Nautilus"],
+    10: ["I hate Grobs, I need a planet with the least amount of them, suggestions?", "Taurino"],
+    11: ["Are Grobs friendly?", "No"],
+    12: ["What's the weather like in Nautilus?", "Clear"],
+    13: ["I'm a Grob, is Asperion a good choice for me to move?", "No"],
+    14: ["Is it true that Feradris always rains?", "yes"],
+    15: ["There's a code 0100 on my rocket. What does it mean?", "Your seatbelt is off"],
+    16: ["I love medicine and looking to work in the medical field. Where should I go?", "Taurino"],
+    17: ["I applied to visit Taurus, they sent me a spacemail with two squiggly lines, what does it mean?", "You are permitted to travel there"],
+    18: ["Got a spacemail of a right angle and a dot under it. What do i do?", "Wait for further instructions and information via spacemail"],
+    19: ["Got a spacemail of an incomplete circle, almost like a 'u'. What do I do?", "Visit www.myspacetravel-permits.glc"],
+    20: ["There's an aggresive human and I've managed to lock them in my basement, who should I contact?", "Call security personnel at $$$"],
+    21: ["What do we do with humans?", "Immediately dispose"]
+}
