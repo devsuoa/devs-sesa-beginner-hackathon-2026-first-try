be4ec19cdfe3605134242b72d85cdadfe4c45bf8
dadpenguin <franklinbula.100@gmail.com>
2026-04-19 00:22:03 +1200

    added login page

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from "react"
+
+export default function Page () {
+    return <div>
+        <Group>
+
+
+            This is login page
+            
+            <InputField/>
+            <InputField/>
+
+
+            <Button/>
+            </Group>
+    </div>
+}
+
+
+
+interface GroupProps {
+    children: ReactNode
+}
+
+function Group (props: GroupProps) {
+    return <div className="flex flex-col gap-2"> 
+        {props.children}
+    </div>
+}
+
+
+function InputField () {
+    return <input className="w-fit border-2" placeholder="input field">
+
+    </input>
+}
+
+function Button () {
+    return <button className=" w-fit p-2 border rounded-xl">
+        Login
+    </button>
+}
